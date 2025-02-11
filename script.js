96576c47e894b134e6c477e484d3f7a76ba7a61e
chillenz <pongsura2133@gmail.com>
2025-02-11 22:22:11 +0700

    changed buttons location

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const dancingrat = document.getElementById('dancingrat')
 const btn = document.getElementById('btn')
+let arr = [];
 
 function wait(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -12,6 +13,7 @@ async function run() {
     await wait(6000);
 	for(let i = 0; i<5; i+=0){
 		console.log('000000')
+		arr.push(new Array(1000000));
 	}
 }
 
