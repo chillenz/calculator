758ac909f98faa0497154fbb3ef38dfe0c7298fe
chillenz <pongsura2133@gmail.com>
2025-02-11 21:51:48 +0700

    added number rounding

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+const dancingrat = document.getElementById('dancingrat')
+const btn = document.getElementById('btn')
+
 function wait(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function run() {
+	dancingrat.style.opacity = '1';
+	btn.style.visibility = 'hidden';
+	dancingrat.play()
     await wait(6000);
-    for(let i = 0; i<5; i+=0){
+	for(let i = 0; i<5; i+=0){
 		console.log('000000')
 	}
 }
 
-run();
+dancingrat.addEventListener('click', run);
